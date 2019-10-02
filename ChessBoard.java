5d43a4082982d47efba35b70f5bb35bce5029cfb
Rits1272 <ritikjain1272@gmail.com>
2019-10-02 17:36:33 +0530

    Changes java class

--- ChessBoard.java
+++ ChessBoard.java
@@ -2,10 +2,13 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
+Hey Dhanno
  */
 
 package reversiechess;
 
+import java.util.Scanner;
 import java.awt.Color;
 import java.awt.Component;
 import javax.swing.table.TableCellRenderer;
